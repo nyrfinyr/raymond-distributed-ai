43475353eab3cefac6fb2758abec0523c296d6ce
Alessio Valenza <alesvale97@gmail.com>
2025-12-01 18:24:03 +0100

    git init

---
+++ src/main/java/it/alesvale/distributed_ai/DistributedAiApplication.java
@@ -0,0 +1,13 @@
+package it.alesvale.distributed_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedAiApplication.class, args);
+	}
+
+}
